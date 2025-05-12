f08374da43934968a67f7532dab3ebd7ef141ba5
FrankXav <framirezl1701@alumno.ipn.mx>
2025-05-11 19:04:36 -0600

    Agregar ruta realtiva a import

--- Codigo/Braille/imprimirTexto.py
+++ Codigo/Braille/imprimirTexto.py
@@ -1,9 +1,11 @@
 import sys
 import os
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from Braille.tansBraille import *
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 
 texto = sys.argv[1]
 
