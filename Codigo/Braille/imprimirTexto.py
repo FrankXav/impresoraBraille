449343a1f0fa5666ec1b483bf020eebf96d3a5c0
FrankXav <framirezl1701@alumno.ipn.mx>
2025-05-11 19:35:42 -0600

    Agregar impresiones de cwd

--- Codigo/Braille/imprimirTexto.py
+++ Codigo/Braille/imprimirTexto.py
@@ -1,7 +1,10 @@
 import sys
 import os
 
-print(os.getcwd())
+print(f'os.getcwd() desde imprimirTexto.py {os.getcwd()}')
+
+# Añadir el directorio raíz al path
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from Braille.tansBraille import *
 
--- Codigo/Test/imprimirTextoBraille.py
+++ Codigo/Test/imprimirTextoBraille.py
@@ -2,10 +2,7 @@ import subprocess
 import os
 import sys
 
-print(os.getcwd())
-
-# Agregar el directorio raíz al path de Python
-sys.path.insert(0, os.getcwd())
+print(f'os.getcwd() desde imprimirTexto.py {os.getcwd()}')
 
 # Ahora ejecutar el script
 subprocess.run(["python", "Braille/imprimirTexto.py", "oficina"])
