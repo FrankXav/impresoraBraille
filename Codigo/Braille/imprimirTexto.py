ccbb300b743670393528bf272817d809b642c2c8
FrankXav <framirezl1701@alumno.ipn.mx>
2025-05-11 19:41:17 -0600

    Quitar caracter especial

--- Codigo/Braille/imprimirTexto.py
+++ Codigo/Braille/imprimirTexto.py
@@ -3,7 +3,6 @@ import os
 
 print("os.getcwd() desde imprimirTexto.py" + str(os.getcwd()))
 
-# Añadir el directorio raíz al path
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from Braille.tansBraille import *
--- Codigo/Test/imprimirTextoBraille.py
+++ Codigo/Test/imprimirTextoBraille.py
@@ -4,5 +4,4 @@ import sys
 
 print("os.getcwd() desde imprimirTexto.py" + str(os.getcwd()))
 
-# Ahora ejecutar el script
 subprocess.run(["python", "Braille/imprimirTexto.py", "oficina"])
