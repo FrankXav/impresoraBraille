db4bac118d04a628fea4d32fa4d82adaf855b5dd
FrankXav <framirezl1701@alumno.ipn.mx>
2025-05-25 13:22:41 -0600

    Aumentar desplazamineto carril

--- Codigo/Electronica/impresionCaracter.py
+++ Codigo/Electronica/impresionCaracter.py
@@ -29,10 +29,12 @@ def impresionCaracter(arregloCaracter):
                 #print("Mover Izquierda")
                 mover1mmIzquierda()
                 mover1mmIzquierda()
+                mover1mmIzquierda()
                 time.sleep(2)
 
             if(columna == 1 and fila !=2):
                 #print("Mover derecha")
+                mover1mmDerecha()
                 mover1mmDerecha()
                 mover1mmDerecha()
                 time.sleep(2)
