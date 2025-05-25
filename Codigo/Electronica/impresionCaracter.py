e10a0ae3df8a15b8260f0fb091f0acc6d115aa59
FrankXav <framirezl1701@alumno.ipn.mx>
2025-05-25 13:25:10 -0600

    Activar actuador

--- Codigo/Electronica/impresionCaracter.py
+++ Codigo/Electronica/impresionCaracter.py
@@ -17,13 +17,13 @@ def impresionCaracter(arregloCaracter):
 
             if(arregloCaracter[columna][fila] == 1):
                 #print("Activar actuador")
-                """ GPIO.output(actuador, GPIO.LOW)
+                GPIO.output(actuador, GPIO.LOW)
                 GPIO.output(Q1, GPIO.LOW)
                 GPIO.output(Q2, GPIO.LOW)
                 GPIO.output(Q3, GPIO.LOW)
                 GPIO.output(Q4, GPIO.LOW)
                 time.sleep(0.3)
-                GPIO.output(actuador, GPIO.HIGH) """
+                GPIO.output(actuador, GPIO.HIGH)
 
             if(columna == 0 and fila !=2):
                 #print("Mover Izquierda")
