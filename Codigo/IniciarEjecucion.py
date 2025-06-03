f88458d303aa1175a5d62a0202d8cb3e5547afec
FrankXav <framirezl1701@alumno.ipn.mx>
2025-06-03 10:49:56 -0600

    print en iniciar Ejecucion

--- Codigo/IniciarEjecucion.py
+++ Codigo/IniciarEjecucion.py
@@ -3,4 +3,6 @@ import time
 
 subprocess.run(["tmux", "new-session", "-d", "-s", "impresorabraille"])
 
-subprocess.run(["tmux", "send-keys", "-t", "impresorabraille", "python3.8 main.py", "ENTER"])
+print("Inicio de terminal")
+
+subprocess.run(["tmux", "send-keys", "-t", "impresorabraille", "python3.8 /home/jetson/Documents/impresoraBraille/Codigo/main.py", "ENTER"])
