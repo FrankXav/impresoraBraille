1e3b5e0c7f154b8e91e0b8cc80d47231c6312cc5
FrankXav <framirezl1701@alumno.ipn.mx>
2025-06-03 06:18:58 -0600

    Comenzar programa arranque

---
+++ Codigo/Arranque/IniciarTerminal.py
@@ -0,0 +1,8 @@
+import subprocess
+import time
+
+subprocess.run(["tmux", "new-session", "-d", "-s", "impresorabraille"])
+
+time.sleep(3)
+
+subprocess.run(["tmux", "send-keys", "-t", "impresorabraille", "ls -l", "ENTER"])
