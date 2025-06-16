3b0aa044a9d3a575dad57fe69e74adcc2b2fd768
FrankXav <framirezl1701@alumno.ipn.mx>
2025-06-15 20:12:31 -0600

    Agregar test avance

--- Codigo/Test/pruebaAvance.py
+++ Codigo/Test/pruebaAvance.py
@@ -1,19 +1,64 @@
-import sys
-import os
+import Jetson.GPIO as GPIO
+import time
 
-from Codigo.Electronica.avanceCinta import *
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 
-GPIO.output(C1, GPIO.LOW)
-GPIO.output(C2, GPIO.LOW)
-GPIO.output(C3, GPIO.LOW)
-GPIO.output(C4, GPIO.LOW)
+secuencia = [[1,0,0,1],
+             [1,0,1,0],
+             [0,1,1,0],
+             [0,1,0,1]]
+
+timeCiclo = 0.0005
+
+C1 = 19
+C2 = 21
+C3 = 22
+C4 = 23
+
+GPIO.setup(C1, GPIO.OUT, initial=0)
+GPIO.setup(C2, GPIO.OUT, initial=0)
+GPIO.setup(C3, GPIO.OUT, initial=0)
+GPIO.setup(C4, GPIO.OUT, initial=0)
+
+def ejecutarCicloAvance(CW):
+    #print("Ejecutar paso")
+    
+    for ind in range(4):
+        
+        if(CW):
+            paso = ind
+        else:
+            paso = 3 - ind
+            
+        #print(secuencia[paso])
+        
+        #Envio de datos
+        GPIO.output(C1, GPIO.HIGH if secuencia[paso][0] == 1 else GPIO.LOW)
+        GPIO.output(C2, GPIO.HIGH if secuencia[paso][1] == 1 else GPIO.LOW)
+        GPIO.output(C3, GPIO.HIGH if secuencia[paso][2] == 1 else GPIO.LOW)
+        GPIO.output(C4, GPIO.HIGH if secuencia[paso][3] == 1 else GPIO.LOW)
+        
+        time.sleep(timeCiclo)
+
+
+def mover1mmDerechaAvance():
+    
+    for i in range(25):
+        ejecutarCicloAvance(CW = True) 
+        #time.sleep(0.1)
+        
+    
+def mover1mmIzquierdaAvance():
+    
+    for i in range(25):
+        ejecutarCicloAvance(CW = False) 
+
 
 while(True):
 
-    print("Mover derecha!")
-    for i in range(10):
+    for i in range(20):
         mover1mmDerechaAvance()
 
     GPIO.output(C1, GPIO.LOW)
@@ -23,9 +68,7 @@ while(True):
 
     time.sleep(2)
 
-    print("Mover izquierda!")
-
-    for i in range(10):
+    for i in range(20):
         mover1mmIzquierdaAvance()
 
     GPIO.output(C1, GPIO.LOW)
