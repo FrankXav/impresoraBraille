4311d49a9dc2fcc50e7ef7199c2983db272055fe
FrankXav <framirezl1701@alumno.ipn.mx>
2025-06-18 18:20:43 -0600

    Agregar prueba carril individual

--- Codigo/Test/pruebaCarril.py
+++ Codigo/Test/pruebaCarril.py
@@ -1,22 +1,54 @@
-from Codigo.Electronica.carril import *
-
-""" for i in range(200):
-    mover1mmDerecha()
+import Jetson.GPIO as GPIO
+import time
 
 
-for i in range(200):
-    mover1mmIzquierda()
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
 
+Q1 = 7
+Q2 = 11
+Q3 = 13
+Q4 = 12
 
-for i in range(200):
-    ejecutarCiclo(CW = True) 
-    #time.sleep(0.1)
+secuencia = [[1,0,0,1],
+             [1,0,1,0],
+             [0,1,1,0],
+             [0,1,0,1]]
+
+timeCiclo = 0.0005
+
+def ejecutarCiclo(CW):
+    #print("Ejecutar paso")
+    
+    for ind in range(4):
         
-GPIO.output(Q1, GPIO.LOW)
-GPIO.output(Q2, GPIO.LOW)
-GPIO.output(Q3, GPIO.LOW)
-GPIO.output(Q4, GPIO.LOW) """
+        if(CW):
+            paso = ind
+        else:
+            paso = 3 - ind
+            
+        #print(secuencia[paso])
+        
+        #Envio de datos
+        GPIO.output(Q1, GPIO.HIGH if secuencia[paso][0] == 1 else GPIO.LOW)
+        GPIO.output(Q2, GPIO.HIGH if secuencia[paso][1] == 1 else GPIO.LOW)
+        GPIO.output(Q3, GPIO.HIGH if secuencia[paso][2] == 1 else GPIO.LOW)
+        GPIO.output(Q4, GPIO.HIGH if secuencia[paso][3] == 1 else GPIO.LOW)
+        
+        time.sleep(timeCiclo)
 
+
+def mover1mmDerecha():
+    
+    for i in range(20):
+        ejecutarCiclo(CW = True) 
+        #time.sleep(0.1)
+        
+    
+def mover1mmIzquierda():
+    
+    for i in range(20):
+        ejecutarCiclo(CW = False) 
 
 while(True):
 
