6d0ec7e9a29a02e10ffeeb0f7e60787fe7bba276
FrankXav <framirezl1701@alumno.ipn.mx>
2025-06-20 17:57:12 -0600

    Duplicar espacio avance

--- Codigo/Electronica/impresionCaracter.py
+++ Codigo/Electronica/impresionCaracter.py
@@ -32,10 +32,14 @@ def impresionCaracter(arregloCaracter):
                 mover1mmIzquierda()
                 mover1mmIzquierda()
                 mover1mmIzquierda()
+                mover1mmIzquierda()
+                mover1mmIzquierda()
                 
 
             if(columna == 1 and fila !=2):
                 #print("Mover derecha")
+                mover1mmDerecha()
+                mover1mmDerecha()
                 mover1mmDerecha()
                 mover1mmDerecha()
                 mover1mmDerecha()
