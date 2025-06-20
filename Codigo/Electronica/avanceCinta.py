56fc0bd3560b3b959b2824c3cc8875e6f50bb24b
FrankXav <framirezl1701@alumno.ipn.mx>
2025-06-20 17:44:31 -0600

    Disminuir Avance

--- Codigo/Electronica/avanceCinta.py
+++ Codigo/Electronica/avanceCinta.py
@@ -33,12 +33,12 @@ def ejecutarCicloAvance(CW):
 
 def mover1mmDerechaAvance():
     
-    for i in range(3):
+    for i in range(1):
         ejecutarCicloAvance(CW = True) 
         #time.sleep(0.1)
         
     
 def mover1mmIzquierdaAvance():
     
-    for i in range(3):
+    for i in range(1):
         ejecutarCicloAvance(CW = False) 
