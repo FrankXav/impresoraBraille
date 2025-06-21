7c55c623d6ce2eecae2fc3244dfbe6da948e5f50
FrankXav <framirezl1701@alumno.ipn.mx>
2025-06-20 18:45:28 -0600

    Agregar avance de cinta 10 pasos final

--- Codigo/Electronica/botones.py
+++ Codigo/Electronica/botones.py
@@ -14,5 +14,5 @@ estado_BotImp = GPIO.input(botonImpresion)
 
 while(GPIO.input(botonImpresion) == 1):
     estado_BotImp = GPIO.input(botonImpresion)
-    print("No presionado")
+    #print("No presionado")
 
