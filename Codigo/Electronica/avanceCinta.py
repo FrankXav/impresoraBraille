9ec27b79834a7560831c3a2870a532fdc2c199ff
FrankXav <framirezl1701@alumno.ipn.mx>
2025-06-30 15:12:35 -0600

    Aumentar paso motor

--- Codigo/Electronica/avanceCinta.py
+++ Codigo/Electronica/avanceCinta.py
@@ -33,12 +33,12 @@ def ejecutarCicloAvance(CW):
 
 def mover1mmDerechaAvance():
     
-    for i in range(1):
+    for i in range(2):
         ejecutarCicloAvance(CW = False) 
         #time.sleep(0.1)
         
     
 def mover1mmIzquierdaAvance():
     
-    for i in range(1):
+    for i in range(2):
         ejecutarCicloAvance(CW = True) 
