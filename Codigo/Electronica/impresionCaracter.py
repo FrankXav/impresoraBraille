4ad9c41b19b442045553fa2ac93a76d412fc0233
FrankXav <framirezl1701@alumno.ipn.mx>
2025-06-30 23:42:38 -0600

    Aumentar timpo caracter act punzon

--- Codigo/Electronica/impresionCaracter.py
+++ Codigo/Electronica/impresionCaracter.py
@@ -23,9 +23,9 @@ def impresionCaracter(arregloCaracter):
                 GPIO.output(Q2, GPIO.LOW)
                 GPIO.output(Q3, GPIO.LOW)
                 GPIO.output(Q4, GPIO.LOW)
-                time.sleep(0.1)
-                GPIO.output(actuador, GPIO.HIGH)
                 time.sleep(0.2)
+                GPIO.output(actuador, GPIO.HIGH)
+                time.sleep(0.4)
 
             if(columna == 0 and fila !=2):
                 #print("Mover Izquierda")
